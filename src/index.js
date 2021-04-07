61ee8673254a0a7503298acc464c93de252a4d2c
aguscorvo <aguscorvo@gmail.com>
2021-04-07 16:51:35 -0300

    Creating images with JavaScript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const MAX = 122;
+const MIN = 1;
 
-console.log('Happy hacking :)')
+const random = () => Math.floor(Math.random() * (MAX - MIN) + MIN);
+
+const createImageNode = () => {
+  //contenedor
+  const container = document.createElement('div');
+  container.className = 'p-4';
+
+  //imagen
+  const image = document.createElement('img');
+  image.className = 'mx-auto';
+  image.style.width = '320px';
+  image.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+  //agregamos imagen al container
+  container.appendChild(image);
+  return container;
+};
+
+// const newImage = createImageNode();
+const mountNode = document.getElementById('images');
+const addButton = document.querySelector('button');
+
+const addImage = () => {
+  const newImage = createImageNode();
+  mountNode.append(newImage);
+};
+
+addButton.addEventListener('click', addImage);
